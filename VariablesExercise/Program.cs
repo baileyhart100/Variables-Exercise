c54682c0c7872522ccd42f10996fc7af73413371
Bailey Hart <baileyhart100@gmail.com>
2023-04-19 16:01:10 -0400

    init commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declare and Initialize Variables
+            string name = "Bailey";
+            int age = 25;
+            char middleInitial = 'L';
+            bool isOver18 = true;
+            double currentTemp = 66.2;
+            decimal currentPrice = 3.89m;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
+
         }
     }
 }
